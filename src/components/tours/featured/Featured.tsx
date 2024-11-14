01d550b348922c5b247d9f93a511bbe1387c860f
Mahedul-Karim <karimrupo21@gmail.com>
2024-11-14 16:56:36 +0600

    card bug fix

--- src/components/tours/featured/Featured.tsx
+++ src/components/tours/featured/Featured.tsx
@@ -4,10 +4,10 @@ import Card from "../Card";
 const Featured = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-2 xs:gap-4 sm:gap-6 lg:gap-10 mt-6">
+      {/* <Card />
       <Card />
       <Card />
-      <Card />
-      <Card />
+      <Card /> */}
     </div>
   );
 };
